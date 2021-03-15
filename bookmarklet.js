ValerioLyndon/MyAnimeList-Tools

commit a57d3a769eca2612fe7a8e0646a1e83d1c6a5adb

Fix entire thang being bugged.
Also simplified a bit of code while looking for the bug, so I kept it in this commit.

bookmarklet.js
@@ -220,11 +220,14 @@ function ProcessNext()
 			{
 				if(modernStyle)
 				{
-					tagEl = document.getElementsByClassName("tags-" + id);
-					if(tagEl.length > 0)
+					tagEl = document.getElementById("tags-" + id);
+					if(tagEl)
 					{
-						tagEl = tagEl[0].children;
-						for(j = 0; j< tagEl.length; j++) { tags.push(tagEl[j].getElementsByTagName("a")[0].textContent); }
+						tagEls = tagEl.querySelectorAll('div a');
+						for(j = 0; j< tagEls.length; j++)
+						{
+							tags.push(tagEls[j].textContent);
+						}
 					}
 					else
 					{
@@ -247,9 +250,10 @@ function ProcessNext()
 			}
 			tagsLength = tags.length;
 			
+			/* remove extra whitespace */
 			for(j = 0; j < tagsLength; j++)
 			{
-				tags[j] = tags[j].replace(/^\s+|\s+$/g, "");
+				tags[j] = tags[j].trim();
 			}
 			
 			/* alternate titles */
@@ -444,9 +448,9 @@ function ProcessNext()
 			genresRaw = $(doc).find('[itemprop="genre"]');
 			if(genresRaw.length !== 0)
 			{
-				for(i = 0; i < genresRaw.length; i++)
+				for(j = 0; j < genresRaw.length; j++)
 				{
-					genres[i] = genresRaw.eq(i).text().trim();
+					genres[j] = genresRaw.eq(j).text().trim();
 
 					/* for(k = 0; k < tagsLength; k++)
 					{
@@ -461,6 +465,7 @@ function ProcessNext()
 			}
 			
 			/* rank */
+			rankHtml = "";
 			rankHtmlStartIndex = str.indexOf(RANK_START);
 			if(rankHtmlStartIndex != -1)
 			{
@@ -579,7 +584,14 @@ function Process()
 	
 	result.value += "\/* Generated by MAL List Tool http://burntjello.webs.com\nTemplate=" + template.value.replace(/\*\//g, "*[DEL]/") + "\nMatchTemplate=" + matchTemplate.value + "\n*\/\n";
 	
-	if(modernStyle) { ids = $("tr.more-info").map(function () { return this.id.replace("more-", ""); } ).get() ; } else { ids = $("div.hide").map(function () { return this.id.replace("more", ""); } ).get() ; }
+	if(modernStyle)
+	{
+		ids = $("tr.more-info").map(function () { return this.id.replace("more-", ""); } ).get() ;
+	}
+	else
+	{
+		ids = $("div.hide").map(function () { return this.id.replace("more", ""); } ).get() ;
+	}
 	
 	idsLength = ids.length;
 	for(k = 0; k < idsLength; k++)

commit cae98785a6def91ea31746ad202d80164bd4f608

Fix tags including "Edit" text on modern.
Regression from commit a57d3a769eca2612fe7a8e0646a1e83d1c6a5adb

bookmarklet.js
@@ -199,7 +199,7 @@ function ProcessNext()
 			{
 				if(modernStyle)
 				{
-					tagEl = document.getElementById("tags-" + id);
+					tagEl = document.querySelector(`.tags-${id}`);
 					if(tagEl)
 					{
 						tagEls = tagEl.querySelectorAll('div a');