ValerioLyndon/MyAnimeList-Tools

commit 3b8ab673c9b26bfcb01e63d3147c5c62e4226690

minor naming tweaks

bookmarklet.js
@@ -315,7 +315,7 @@ thumbBtn.classList.add('burnt-btn');
 thumbBtn.type = "button";
 thumbBtn.value = "Loading...";
 thumbBtn.disabled = 'disabled';
-thumbBtn.onclick = function() { Process(); };
+thumbBtn.onclick = function() { beginProcessing); };
 
 exitBtn = document.createElement("input");
 guiL.append(exitBtn);
@@ -1083,7 +1083,7 @@ var timeout;
 
 iteration = 0;
 timeThen = performance.now() - delay.value;
-function ProcessNext()
+function continueProcessing()
 {
 	if(iteration < newData.length)
 	{
@@ -1783,15 +1783,15 @@ function ProcessNext()
 
 		timeText.textContent = `~ ${timeRemaining} left`;
 		
-		timeout = setTimeout(ProcessNext, delay.value);
+		timeout = setTimeout(continueProcessing, delay.value);
 	}
 	else
 	{
-		DoneProcessing();
+		finishProcessing();
 	}
 }
 
-function DoneProcessing()
+function finishProcessing()
 {
 	if(result.value.length > 0)
 	{
@@ -1829,7 +1829,7 @@ function DoneProcessing()
 	};
 }
 
-function Process()
+function beginProcessing)
 {
 	saveSettings();
 
@@ -1846,7 +1846,7 @@ function Process()
 		thumbBtn.disabled = 'disabled';
 		newData = [];
 		clearTimeout(timeout);
-		DoneProcessing();
+		finishProcessing();
 	};
 	
 	result.value += `\/*\nGenerated by MyAnimeList-Tools v${ver}\nhttps://github.com/ValerioLyndon/MyAnimeList-Tools\n\nTemplate=${template.value.replace(/\*\//g, "*[DEL]/")}\nMatchTemplate=${matchTemplate.value}\n*\/\n\n`;
@@ -1901,7 +1901,7 @@ function Process()
 		}
 	}
 
-	timeout = setTimeout(ProcessNext, imageLoadDelay);
+	timeout = setTimeout(continueProcessing, imageLoadDelay);
 }
 
 function saveSettings()

commit b28635329a12d9b60827451c0f11bfba9fb4085e

fix busted function
regression from 3b8ab673c9b26bfcb01e63d3147c5c62e4226690

bookmarklet.js
@@ -315,7 +315,7 @@ thumbBtn.classList.add('burnt-btn');
 thumbBtn.type = "button";
 thumbBtn.value = "Loading...";
 thumbBtn.disabled = 'disabled';
-thumbBtn.onclick = function() { beginProcessing); };
+thumbBtn.onclick = function() { beginProcessing(); };
 
 exitBtn = document.createElement("input");
 guiL.append(exitBtn);
@@ -1829,7 +1829,7 @@ function finishProcessing()
 	};
 }
 
-function beginProcessing)
+function beginProcessing()
 {
 	saveSettings();
 