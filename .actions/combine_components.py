5fdfeaa76531f51f67ddb6d830796e1c64d01340
Valerio Lyndon <valeriolyndon@gmail.com>
2023-06-10 13:50:04 -0700

    split CSS into it's own file

--- .actions/combine_components.py
+++ .actions/combine_components.py
@@ -1,65 +1,67 @@
 import os
+import re
 
-# get script path
+# FUNCTIONS
+
+# remove whitespace for testing purposes
+def removeLines(text):
+	return re.sub(r"[\n\r]", '', text)
+
+# PROGRAM
+
+# set cwd to repo base path
 script_path = os.path.abspath(__file__)
-# set repo base path
 base_path = os.path.dirname(os.path.dirname(script_path))
-# set cwd
 os.chdir(base_path)
 
 def path(sub_path):
 	return os.path.join(base_path, sub_path)
 
-textRecipes = []
+textReplacements = []
 with open(path('components/variables'), 'r') as file:
 	for var in file.readlines():
 		name = var.split('=')[0].lstrip()
 		value = var.split('=')[1].rstrip('\n\r')
-		textRecipes.append([name, value])
+		textReplacements.append([name, value])
+
+fileReplacements = [
+	['credit', 'components/credit.js'],
+	['store', 'components/store.js'],
+	['main', 'components/main.js'],
+	['css', 'components/primary.css']
+]
 
 fileRecipes = [
 	{
-		'base': 'components/userscript-base.js',
-		'combine': [
-			['credit', 'components/credit.js'],
-			['store', 'components/store.js'],
-			['main', 'components/main.js']
-		],
+		'in': 'components/userscript-base.js',
 		'out': 'script.user.js'
 	},
 	{
-		'base': 'components/bookmarklet-base.js',
-		'combine': [
-			['credit', 'components/credit.js'],
-			['store', 'components/store.js'],
-			['main', 'components/main.js']
-		],
+		'in': 'components/bookmarklet-base.js',
 		'out': 'bookmarklet.js'
+	},
+	{
+		'in': 'bookmarklet.js',
+		'out': 'bookmarklet-test.nfp.js',
+		'func': removeLines
 	}
 ]
 
 def replaceVars(text):
-	for replacement in textRecipes:
+	for replacement in fileReplacements:
+		with open(path(replacement[1]), 'r') as file:
+			replacementText = file.read()
+			text = text.replace(f"/*<<<{replacement[0]}>>>*/", replacementText)
+	for replacement in textReplacements:
 		text = text.replace(f"/*$$${replacement[0]}$$$*/", replacement[1])
 	return text
 
 for recipe in fileRecipes:
-	with open(path(recipe['base']), 'r') as file:
+	with open(path(recipe['in']), 'r') as file:
 		text = replaceVars(file.read())
-	
-	for combination in recipe['combine']:
-		with open(path(combination[1]), 'r') as file:
-			newText = replaceVars(file.read())
-			text = text.replace(f"/*<<<{combination[0]}>>>*/", newText)
+
+	if 'func' in recipe:
+		text = recipe['func'](text)
 	
 	with open(path(recipe['out']), 'w+') as file:
 		file.write(text)
-
-# Create bookmarklet version without whitespace for testing purposes
-
-import re
-with open(path('bookmarklet.js'), 'r') as infile:
-	with open(path('bookmarklet-test.nfp.js'), 'w') as outfile:
-		text = infile.read()
-		text = re.sub(r"[\n\r]", '', text)
-		outfile.write(text)
