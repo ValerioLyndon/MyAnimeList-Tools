b052c3c7c3ab3686bfcd5f7f1261a6de895d7873
Valerio Lyndon <valeriolyndon@gmail.com>
2023-06-10 12:07:05 -0700

    discern repo base path instead of relying on cwd

--- .actions/combine_components.py
+++ .actions/combine_components.py
@@ -1,5 +1,17 @@
+import os
+
+# get script path
+script_path = os.path.abspath(__file__)
+# set repo base path
+base_path = os.path.dirname(os.path.dirname(script_path))
+# set cwd
+os.chdir(base_path)
+
+def path(sub_path):
+	return os.path.join(base_path, sub_path)
+
 textRecipes = []
-with open('../components/variables', 'r') as file:
+with open(path('components/variables'), 'r') as file:
 	for var in file.readlines():
 		name = var.split('=')[0].lstrip()
 		value = var.split('=')[1].rstrip('\n\r')
@@ -7,22 +19,22 @@ with open('../components/variables', 'r') as file:
 
 fileRecipes = [
 	{
-		'base': '../components/userscript-base.js',
+		'base': 'components/userscript-base.js',
 		'combine': [
-			['credit', '../components/credit.js'],
-			['store', '../components/store.js'],
-			['main', '../components/main.js']
+			['credit', 'components/credit.js'],
+			['store', 'components/store.js'],
+			['main', 'components/main.js']
 		],
-		'out': '../script.user.js'
+		'out': 'script.user.js'
 	},
 	{
-		'base': '../components/bookmarklet-base.js',
+		'base': 'components/bookmarklet-base.js',
 		'combine': [
-			['credit', '../components/credit.js'],
-			['store', '../components/store.js'],
-			['main', '../components/main.js']
+			['credit', 'components/credit.js'],
+			['store', 'components/store.js'],
+			['main', 'components/main.js']
 		],
-		'out': '../bookmarklet.js'
+		'out': 'bookmarklet.js'
 	}
 ]
 
@@ -32,22 +44,22 @@ def replaceVars(text):
 	return text
 
 for recipe in fileRecipes:
-	with open(recipe['base'], 'r') as file:
+	with open(path(recipe['base']), 'r') as file:
 		text = replaceVars(file.read())
 	
 	for combination in recipe['combine']:
-		with open(combination[1], 'r') as file:
+		with open(path(combination[1]), 'r') as file:
 			newText = replaceVars(file.read())
 			text = text.replace(f"/*<<<{combination[0]}>>>*/", newText)
 	
-	with open(recipe['out'], 'w+') as file:
+	with open(path(recipe['out']), 'w+') as file:
 		file.write(text)
 
 # Create bookmarklet version without whitespace for testing purposes
 
 import re
-with open('../bookmarklet.js', 'r') as infile:
-	with open('../bookmarklet-test.nfp.js', 'w') as outfile:
+with open(path('bookmarklet.js'), 'r') as infile:
+	with open(path('bookmarklet-test.nfp.js'), 'w') as outfile:
 		text = infile.read()
 		text = re.sub(r"[\n\r]", '', text)
 		outfile.write(text)
