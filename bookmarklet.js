ValerioLyndon/MyAnimeList-Tools

commit cd2a55b010fee724c6a6db870ecc46ef48be0421

move user settings into its own class

components/variables
@@ -1,4 +1,4 @@
-ver_core=10.2
+ver_core=11.0-pre
 ver_user=0
 ver_book=0
-ver_date=2023/Jun/18
+ver_date=2023/Aug/17

commit 9221873c719b55740bb9db70ae153807504a6cda

fix new settings not being applied to older
regression from cd2a55b010fee724c6a6db870ecc46ef48be0421

bookmarklet.js
@@ -7,7 +7,7 @@ MyAnimeList-Tools
 - Further changes 2021+       by Valerio Lyndon
 */
 
-const ver = '11.0-pre16_b0';
+const ver = '11.0-pre17_b0';
 const verMod = '2023/Aug/25';
 
 class CustomStorage {
@@ -461,7 +461,7 @@ class UserSettings {
 			
 				/* Check for missing settings and fill them in. This prevents errors while maintaining user settings, especially in the case of a user updating from an older version. */
 				for( let [key, value] of Object.entries(this.settings) ){
-					if( !(key in this.settings) ){
+					if( !(key in workspace) ){
 						workspace[key] = this.settings[key];
 					}
 					if( isDict(value) ){
@@ -2286,7 +2286,7 @@ class UserInterface {
 	border-radius: 9px;
 }
 .c-component.is-disabled {
-	opacity: 0.5;
+	opacity: 0.7;
 }
 
 .c-option {

components/main.js
@@ -380,7 +380,7 @@ class UserSettings {
 			
 				/* Check for missing settings and fill them in. This prevents errors while maintaining user settings, especially in the case of a user updating from an older version. */
 				for( let [key, value] of Object.entries(this.settings) ){
-					if( !(key in this.settings) ){
+					if( !(key in workspace) ){
 						workspace[key] = this.settings[key];
 					}
 					if( isDict(value) ){

components/variables
@@ -1,4 +1,4 @@
-ver_core=11.0-pre16
+ver_core=11.0-pre17
 ver_user=0
 ver_book=0
 ver_date=2023/Aug/25

script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         List Tools
 // @namespace    V.L
-// @version      11.0-pre16_a0
+// @version      11.0-pre17_a0
 // @description  Provides tools for managing your list's tags, CSS, and more.
 // @author       Valerio Lyndon
 // @match        https://myanimelist.net/animelist/*
@@ -24,7 +24,7 @@ MyAnimeList-Tools
 - Further changes 2021+       by Valerio Lyndon
 */
 
-const ver = '11.0-pre16_a0';
+const ver = '11.0-pre17_a0';
 const verMod = '2023/Aug/25';
 
 class CustomStorage {
@@ -478,7 +478,7 @@ class UserSettings {
 			
 				/* Check for missing settings and fill them in. This prevents errors while maintaining user settings, especially in the case of a user updating from an older version. */
 				for( let [key, value] of Object.entries(this.settings) ){
-					if( !(key in this.settings) ){
+					if( !(key in workspace) ){
 						workspace[key] = this.settings[key];
 					}
 					if( isDict(value) ){
@@ -2303,7 +2303,7 @@ class UserInterface {
 	border-radius: 9px;
 }
 .c-component.is-disabled {
-	opacity: 0.5;
+	opacity: 0.7;
 }
 
 .c-option {